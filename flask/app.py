7ae03ee58bdfee92092327f4f0165ac335712547
YUYUJIN <wlsyou98@gmail.com>
2023-05-23 21:37:24 +0900

    Reflection of requirements

--- flask/app.py
+++ flask/app.py
@@ -39,18 +39,12 @@ import time
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Const
+import json
+with open('./const.json','r') as j:
+    settings=json.load(j)
+# Const
 SAVE_DIR=increment_path(Path(ROOT / 'image'),exist_ok=True)
-SAVE_PATH='/image'
-IMAGE_SIZE=640
-IMAGE_STRIDE=32
-AUTO=True
-CONF_THOLD=0.3
 DEVICE='cuda' if torch.cuda.is_available() else 'cpu'
-labels_dict={0:'top',1:'bottom',2:'outer',3:'onepiece'}
-weather_dict_key=['temp_c','is_day','wind_kph','pressure_mb','humidity','cloud','feelslike_c','precip_mm']
-table_columns=['CATEGORY','IMAGE','TEMP','DAY','WIND','PRESS','HUMI','CLOUD','FEELS','PRECI']
-MAXIUM_VALUE={'temp_c':40,'is_day':1,'wind_kph':20,'pressure_mb':1200,'humidity':100,'cloud':100,'feelslike_c':40,'precip_mm':200}
-RECOMMEND_THOLD=0.8
 
 # For weather API
 import requests
@@ -71,7 +65,7 @@ def index():
     return render_template('index.html')
 
 def preprocess(image,device):
-    im=letterbox(image,IMAGE_SIZE,stride=IMAGE_STRIDE,auto=AUTO)[0] # padded resize
+    im=letterbox(image,settings['IMAGE_SIZE'],stride=settings['IMAGE_STRIDE'],auto=settings['AUTO'])[0] # padded resize
     im=im.transpose((2,0,1))[::-1] # BGR to RGB
     im=np.ascontiguousarray(im) # contiguous
     im=torch.from_numpy(im).to(device)
@@ -85,6 +79,23 @@ def preprocess(image,device):
 def get_image(filename):
     return send_from_directory('./image', filename)
 
+def byCategory(current_state,category,count):
+    options='CATEGORY=\"'+category+"\""
+    clothes_data=db.getData(table_name=os.environ.get('TABLENAME'),option=options)
+    
+    coordinates=[]
+    for i,cd in enumerate(clothes_data):
+        cos_sim=cosine_similarity(np.array([current_state]),np.array([cd[3:]]))
+        if cos_sim>settings['RECOMMEND_THOLD']:
+            coordinates.append([i,cos_sim])
+    coordinates.sort(key=lambda x:x[1])
+    if len(coordinates)<count:
+        result=[[clothes_data[cd[0]][0],clothes_data[cd[0]][2]] for cd in coordinates]
+    else:
+        result=[[clothes_data[cd[0]][0],clothes_data[cd[0]][2]] for cd in coordinates[:count]]
+
+    return result
+
 @app.route('/recommend',methods=['POST'])
 def recommend():
     if request.method=='POST':
@@ -94,29 +105,21 @@ def recommend():
         params={'lat':target['lat'],'lon':target['lon']}
         weather=requests.get(WEATHER_URL,params=params)
         weather_data=literal_eval(weather.content.decode('utf-8'))['current']
-        current_state=[weather_data[key]/MAXIUM_VALUE[key] for key in weather_dict_key]
-        
-        clothes_data=db.getData(table_name=os.environ.get('TABLENAME'))
-        
-        coordinates=[]
-        for i,cd in enumerate(clothes_data):
-            cos_sim=cosine_similarity(np.array([current_state]),np.array([cd[3:]]))
-            if cos_sim>RECOMMEND_THOLD:
-                coordinates.append([i,cos_sim])
-        coordinates.sort(key=lambda x:x[1])
-        if len(coordinates)<10:
-            result=[clothes_data[cd[0]][2] for cd in coordinates]
-        else:
-            result=[clothes_data[cd[0]][2] for cd in coordinates[:10]]
+        current_state=[weather_data[key]/settings['MAXIUM_VALUE'][key] for key in settings['weather_dict_key']]
+
+        results=dict()
+        label_size=settings['RECOMMEND_COUNT']
+        for key in label_size.keys():
+            results[key]=byCategory(current_state=current_state,category=key,count=label_size[key])
 
         response={
-            'images': result,
+            'images': results,
             'message': 'success'
         } 
     
     return jsonify(response),200
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload/today', methods=['POST'])
 def upload():
     if request.method=='POST':
         target=request.get_json()
@@ -141,12 +144,12 @@ def upload():
                 filename=time.strftime("%Y%m%d-%H%M%S-")+str(i)+'.jpg'
                 save_one_box(xyxy, img, file=SAVE_DIR / filename, BGR=False)
 
-                file_path=SAVE_PATH+'/'+filename
-                clss = labels_dict[int(cls)]
+                file_path=settings['SAVE_PATH']+'/'+filename
+                clss = settings['labels_dict'][str(int(cls))]
 
                 insert_data=[clss,file_path]
-                insert_data+=[weather_data[key]/MAXIUM_VALUE[key] for key in weather_dict_key]
-                db.insData(table_name=os.environ.get('TABLENAME'),columns=table_columns,values=insert_data)
+                insert_data+=[weather_data[key]/settings['MAXIUM_VALUE'][key] for key in settings['weather_dict_key']]
+                db.insData(table_name=os.environ.get('TABLENAME'),columns=settings['table_columns'],values=insert_data)
 
         response={
                 'message': 'success'
@@ -154,13 +157,14 @@ def upload():
 
     return jsonify(response),200
 
-@app.route('/upload/beggin',methods=['POST'])
+@app.route('/upload/category',methods=['POST'])
 def uploadB():
     if request.method=='POST':
         target=request.get_json()
 
         # weather api
-        weather_data=[float(target[key]) for key in weather_dict_key]
+        category_data=settings['category'][target['category']]
+        weather_data=[float(category_data[key]) for key in settings['weather_dict_key']]
 
         # image
         img=np.array(list(target['data'].values()), np.uint8).reshape((target['height'],target['width'],target['bpp']))
@@ -177,12 +181,12 @@ def uploadB():
                 filename=time.strftime("%Y%m%d-%H%M%S-")+str(i)+'.jpg'
                 save_one_box(xyxy, img, file=SAVE_DIR / filename, BGR=False)
 
-                file_path=SAVE_PATH+'/'+filename
-                clss = labels_dict[int(cls)]
+                file_path=settings['SAVE_PATH']+'/'+filename
+                clss = settings['labels_dict'][str(int(cls))]
 
                 insert_data=[clss,file_path]
-                insert_data+=[weather_data[i]/MAXIUM_VALUE[key] for i,key in enumerate(weather_dict_key)]
-                db.insData(table_name=os.environ.get('TABLENAME'),columns=table_columns,values=insert_data)
+                insert_data+=[weather_data[i]/settings['MAXIUM_VALUE'][key] for i,key in enumerate(settings['weather_dict_key'])]
+                db.insData(table_name=os.environ.get('TABLENAME'),columns=settings['table_columns'],values=insert_data)
 
         response={
                 'message': 'success'
@@ -190,11 +194,31 @@ def uploadB():
 
     return jsonify(response),200
 
+@app.route('/delete/clothes',methods=['DELETE'])
+def deleteClothes():
+    if request.method=='DELETE':
+        target=request.get_json()
+        
+        try:
+            filename=db.getData(table_name=os.environ.get('TABLENAME'),option='ID=\"'+str(target['id'])+'\"')[0][2]
+            db.delData(table_name=os.environ.get('TABLENAME'),id=target['id'])
+            os.remove('.'+filename)
+
+            response={
+                'message': 'success'
+            }
+        except:
+            response={
+                'message': 'fail'
+            }
+    
+    return jsonify(response),200
+
 if __name__ == '__main__':
     db=Database(host=os.environ.get('HOST'),port=int(os.environ.get('PORT')),user=os.environ.get('USERNAME'),password=os.environ.get('PASSWARD'),database_name=os.environ.get('DATABASENAME'))
 
     #device=select_device(device)
     model=attempt_load(weights_path,device=DEVICE)
-    model.conf=CONF_THOLD
+    model.conf=settings['CONF_THOLD']
 
     app.run(host='0.0.0.0',port=5000,debug=True)
